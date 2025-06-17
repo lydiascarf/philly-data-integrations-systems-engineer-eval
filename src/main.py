80e26bfb29d1aa229bf0dd41a438800ad827a6fd
Lydia Scarf <lydia@oldgrowth.city>
2025-06-17 19:22:27 +0000

    Frontload data preparation to setup script
    Optimizes docker build process by including a join and
    API requests in the setup step. This means that changes
    to src/main.py can build without redoing long-running
    operations.

--- src/main.py
+++ src/main.py
@@ -1,25 +1,9 @@
 import pandas as pd
-import requests
-import urllib.parse
 
-service_requests_df = pd.read_csv('311_requests.csv')
-l_and_i_requests_df = service_requests_df.query("agency_responsible == 'License & Inspections'").sample(n=10)
 
-def request_opa_account_number(address):
-    if not isinstance(address, str):
-        return ''
-    safe_query_string = urllib.parse.quote(address)
-    url = f'https://api.phila.gov/ais/v2/search/{safe_query_string}'
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.json().get('features',[{}])[0].get('properties',{}).get('opa_account_num', '')
-    else:
-        return ''
+def run_analysis():
+    df = pd.read_csv('l_i_requests.csv')
+    print(df)
 
-l_and_i_requests_df['opa_account_num'] = l_and_i_requests_df['address'].apply(request_opa_account_number)
-violations_df = pd.read_csv('violations.csv')
-violations_df['opa_account_num'] = violations_df['opa_account_num'].astype(str)
+run_analysis()
 
-join = pd.merge(violations_df, l_and_i_requests_df, how='outer', on='opa_account_num')
-
-join.to_csv('joined_data.csv', index=False)
--- src/setup.py
+++ src/setup.py
@@ -1,19 +1,22 @@
 import os
 import pandas as pd
+import requests
+import urllib.parse
 
-def get_311_requests_csv():
+def get_service_requests():
     df = None
-    download_path = '311_requests.csv'
+    download_path = 'service_requests.csv'
     if os.path.exists(download_path):
-        print('Cached 311 requests csv found')
-        service_requests_df = pd.read_csv(download_path)
+        print('Cached service requests csv found')
+        df = pd.read_csv(download_path)
     else:
-        print('Downloading 311 requests csv')
+        print('Downloading service requests csv')
         url = "https://phl.carto.com/api/v2/sql?filename=public_cases_fc&format=csv&skipfields=cartodb_id,the_geom,the_geom_webmercator&q=SELECT%20*%20FROM%20public_cases_fc%20WHERE%20requested_datetime%20%3E=%20%272025-01-01%27%20AND%20requested_datetime%20%3C%20%272026-01-01%27"
         df = pd.read_csv(url)
         df.to_csv(download_path, index=False)
+    return df
 
-def get_violations_csv():
+def get_violations():
     df = None
     download_path = 'violations.csv'
     if os.path.exists(download_path):
@@ -24,6 +27,32 @@ def get_violations_csv():
         url = "https://phl.carto.com/api/v2/sql?&filename=violations&format=csv&skipfields=cartodb_id,the_geom,the_geom_webmercator&q=SELECT%20*,%20ST_Y(the_geom)%20AS%20lat,%20ST_X(the_geom)%20AS%20lng%20FROM%20violations%20WHERE%20violationdate%20%3E=%20%272019-01-01%27"
         df = pd.read_csv(url)
         df.to_csv(download_path, index=False)
+    return df
 
-get_311_requests_csv()
-get_violations_csv()
+def request_opa_account_number(address):
+    if not isinstance(address, str):
+        return ''
+    safe_query_string = urllib.parse.quote(address)
+    url = f'https://api.phila.gov/ais/v2/search/{safe_query_string}'
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json().get('features',[{}])[0].get('properties',{}).get('opa_account_num', '')
+    else:
+        return ''
+
+def prepare_l_i_requests():
+    df = None
+    download_path = 'l_i_requests.csv'
+    if os.path.exists(download_path):
+        print('Cached L&I requests csv found')
+    else:
+        service_requests_df = get_service_requests()
+        violations_df = get_violations()
+        l_and_i_requests_df = service_requests_df.query("agency_responsible == 'License & Inspections'").sample(n=500)
+        l_and_i_requests_df['opa_account_num'] = l_and_i_requests_df['address'].apply(request_opa_account_number)
+        violations_df['opa_account_num'] = violations_df['opa_account_num'].astype(str)
+
+        df = pd.merge(violations_df, l_and_i_requests_df, how='right', on='opa_account_num')
+        df.to_csv(download_path, index=False)
+
+prepare_l_i_requests()
